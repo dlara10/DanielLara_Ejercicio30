d7c665be69cee6fe1fc627a635902479573b3d0e
Default user <dlara10>
2019-05-09 12:50:43 +0000

    Ejercicio_30

--- DanielLara_Ejercicio30.cpp
+++ DanielLara_Ejercicio30.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <fstream>
+#include <stdio.h>  
 #include <cmath>
+#include <math.h>  
 
 using namespace std;
 
 int main(){
     int m = 100;
     int c = 1;
-    float dx = 1/m;
-    float beta = 0.8;
+    float dx = 0.01;
     float u[m+1];
     float u0[m+1];
     float uf[m+1];
@@ -17,29 +18,29 @@ int main(){
     int n = Tfinal/dt;
     int i, j;
     float t = 0.0;
-    double pi = asin(1.0)*2.0;
-    int x;
+    double pi = 3.1415;
+    double x;
+    double beta = 1/(dx/dt);
     ofstream outfile;
 
   for(i=0;i<m;i++){
     x = i*dx;
-    u0[i] = sin(pi*x);
+    u0[i] = 0.04*sin(4*pi*x/c);
     u[i] = u0[i];
     uf[i] = u0[i];
   }
   outfile.open("adveccion.dat");
   while(t < Tfinal){
-    for(i=0;i<m;i++){
-      outfile << u[i] << " ";
+    for(j=0;j<m;j++){
+      outfile << u[j] << " ";
     }
     outfile << "\n";
-    
     for(i=1;i<m-1;i++){
-      uf[i] = (1-beta*beta)*u0[i] - (0.5*beta)*(1-beta)*u0[i+1]+(0.5*beta)*(1-beta)*u0[i-1];
-    }
+      uf[i] = u[i] - (beta/4)*(u0[i+1]*u0[i+1]*-u0[i-1]*u0[i-1])+((beta*beta)/8)*((u0[i+1]*u0[i])*(u0[i+1]*u0[i+1]-u0[i]*u0[i])-(u0[i]*u0[i-1])*(u0[i]*u0[i-1]-u0[i]*u0[i-1]));
+     }
     for(i=1;i<m-1;i++){
-      u0[i] = u[i];
       u[i] = uf[i];
+      u0[i] = u[i]; 
     }
     t = t + dt;
   }
--- plot.py
+++ plot.py
@@ -1,28 +1,39 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-
+from matplotlib.animation import FuncAnimation
+import imageio
+import os
 
 fig = plt.figure()
-
 data = np.loadtxt("adveccion.dat")
-print(np.shape(data))
-x = np.shape(data)[0]
-y = np.zeros(x)
-i = 0
-j = 0
-im = []
+x = np.linspace(0.0,1.0,len(data[1]))
+fig, ax = plt.subplots()
+xdata, ydata = [], []
+ln, = plt.plot([], [], 'ro')
+plt.plot(x, data[0])
 
-for i in range(x):
-    x1 = data[i]
-    for j in range(x):
-        y[j] = x1[j]
-        t = np.arange(np.shape(data)[1])
-        im = plt.plot((y,t), animated=True)
-        ims.append([im])
-        
-ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True,repeat_delay=1000)
-ani.save('movie.gif')
+def init():
+    ax.set(xlabel='Posicion[metros]', ylabel = 'U')
+    ax.set_xlim(0.0, 1.0)
+    ax.set_ylim(-0.04, 0.04)
+    return ln,
+
+def update():
+    for i in range(len(data[1])):
+        ln.set_ydata(np.shape(data[i]))
+    return ln,
+
+ani = FuncAnimation(fig, update, frames=np.shape(data)[0],
+                    init_func=init, blit=True)
+ani.save("movie.gif")
 plt.show()
+
+
+
+
+
+
+
+
 
 
